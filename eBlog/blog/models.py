6fe030d1bb4f2670798dc826a03d07c10d07ff72
Vladimir Korchagin <korchagin_vova@mail.ru>
2023-06-08 13:10:35 +0500

    canonical urls , html templates

--- eBlog/blog/models.py
+++ eBlog/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -23,6 +24,10 @@ class Post(models.Model):
 
     objects = models.Manager()
     published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month,
+                                                 self.publish.day, self.slug])
 
 class Meta:
     ordering = ('-publish',)
