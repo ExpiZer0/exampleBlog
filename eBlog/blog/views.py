95770d67df538764c85a04ededc4f7286d913c98
Vladimir Korchagin <korchagin_vova@mail.ru>
2023-06-08 13:36:04 +0500

    feat(blog): added pagination

--- eBlog/blog/views.py
+++ eBlog/blog/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    number_of_posts = 3
+    paginator = Paginator(object_list, number_of_posts) # По 3 статьи на каждой странице.
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Если страница не является целым числом, возвращаем первую страницу.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если номер страницы больше, чем общее количество страниц, возвращаем последнюю.
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
